0edfe7e9cd3ea8e841141ae788d79f9c0b0f3dfd
hvardhan617 <hvardhan617@gmail.com>
2018-05-08 22:44:42 +0530

    Initial import

---
+++ src/main/java/com/example/demo/SpotifyTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyTestApplication.class, args);
+	}
+}
